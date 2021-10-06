0b8201a8d6e5142d1fdcb6d649676558a228ec7a
Rafael Mian <rafaelmian35@gmail.com>
2021-10-06 15:36:29 -0300

    wtf

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,121 +1,104 @@
 // import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import React, { useEffect } from "react";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-import { connect } from "react-redux";
-import Home from "./pages/Home";
-import Terms from "./pages/Terms";
-import Privacy from "./pages/Privacy";
-import NotFound from "./pages/NotFound";
-import Account from "./pages/Account";
-import MenuResponsive from "./components/MenuResponsive";
-import Game from "./pages/Game";
-import usersActions from "./redux/actions/usersActions";
-import otherActions from "./redux/actions/otherActions";
-import GameButtons from "./pages/GameButtons";
-import toast, { Toaster } from "react-hot-toast";
-import AdminPanel from "./pages/Admin";
-import gamesActions from "./redux/actions/gamesActions";
-import Friends from "./pages/Friends";
-import socketActions from "./redux/actions/socketActions";
-import GameMultiPlayer from "./pages/GameMultiPlayer";
+import './App.css'
+import React, { useEffect } from 'react'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
+import Home from './pages/Home'
+import Terms from './pages/Terms'
+import Privacy from './pages/Privacy'
+import NotFound from './pages/NotFound'
+import Account from './pages/Account'
+import MenuResponsive from './components/MenuResponsive'
+import Game from './pages/Game'
+import usersActions from './redux/actions/usersActions'
+import otherActions from './redux/actions/otherActions'
+import GameButtons from './pages/GameButtons'
+import toast, { Toaster } from 'react-hot-toast'
+import AdminPanel from './pages/Admin'
+import gamesActions from './redux/actions/gamesActions'
+import Friends from './pages/Friends'
+import socketActions from './redux/actions/socketActions'
+import GameMultiPlayer from './pages/GameMultiPlayer'
 
 const App = (props) => {
   useEffect(() => {
-    if (localStorage.getItem("token")) {
-      props.logInLS();
-      props.setGame(localStorage.getItem("token"));
+    if (localStorage.getItem('token')) {
+      props.logInLS()
+      props.setGame(localStorage.getItem('token'))
     }
     // eslint-disable-next-line
-  }, []);
+  }, [])
 
   useEffect(() => {
-    if (props.sockek) {
-      props.socket.on("game_request", ({ username, requests }) => {
-        props.setGameRequests(requests, false);
-        toast(username + " invited you to a game!", {
-          icon: "🎮",
-        });
-      });
-      props.socket.on(
-        "accepted_game_request",
-        ({ username, requests, playing_now, game, coins }) => {
-          props.setGameRequests(requests, playing_now);
-          props.setMultiGame(game, coins);
-          toast(username + " accepted your game invitation!", {
-            icon: "🎮",
-          });
-        }
-      );
+    if (props.socket) {
+      props.socket.on('game_request', ({ username, requests }) => {
+        props.setGameRequests(requests, false)
+        toast(username + ' invited you to a game!', {
+          icon: '🎮',
+        })
+      })
+      props.socket.on('accepted_game_request', ({ username, requests, playing_now, game, coins }) => {
+        props.setGameRequests(requests, playing_now)
+        props.setMultiGame(game, coins)
+        toast(username + ' accepted your game invitation!', {
+          icon: '🎮',
+        })
+      })
       // props.socket.on("change_current_player", (username) => { });
-      props.socket.on("friend_request", ({ username, requests }) => {
-        props.setFriendRequests(requests);
-        toast(username + " has sent you a friend request!", {
-          icon: "🤝",
-        });
-      });
-      props.socket.on(
-        "accepted_friend_request",
-        ({ username, requests, friends }) => {
-          props.setFriends(requests, friends);
-          toast(
-            username + " accepted your friend request! Invite him to a game",
-            {
-              icon: "🤝",
-            }
-          );
-        }
-      );
-      props.socket.on("connected", (username) => {
+      props.socket.on('friend_request', ({ username, requests }) => {
+        props.setFriendRequests(requests)
+        toast(username + ' has sent you a friend request!', {
+          icon: '🤝',
+        })
+      })
+      props.socket.on('accepted_friend_request', ({ username, requests, friends }) => {
+        props.setFriends(requests, friends)
+        toast(username + ' accepted your friend request! Invite him to a game', {
+          icon: '🤝',
+        })
+      })
+      props.socket.on('connected', (username) => {
         if (
           username !== props.username &&
           props.userData.friends.length > 0 &&
           props.userData.friends.some((friend) => friend.username === username)
         ) {
-          props.setFriendsList();
+          props.setFriendsList()
         }
-      });
-      props.socket.on("disconnection", (username) => {
+      })
+      props.socket.on('disconnection', (username) => {
         if (
           username !== props.username &&
           props.userData.friends.length > 0 &&
           props.userData.friends.some((friend) => friend.username === username)
         ) {
-          props.setFriendsList();
+          props.setFriendsList()
         }
-      });
+      })
     }
     // eslint-disable-next-line
-  }, [props.socket]);
+  }, [props.socket])
 
   return (
     <BrowserRouter>
       <Toaster />
       {props.menu && <MenuResponsive />}
       <Switch>
-        <Route path="/prueba" component={GameMultiPlayer} />
-        <Route exact path="/" {...props} component={Home} />
-        <Route path="/terms" component={Terms} />
-        <Route path="/privacy" component={Privacy} />
-        <Route path="/notfound" component={NotFound} />
-        <Route path="/game" component={Game} />
-        <Route path="/accounts" component={!props.token ? Account : Home} />
-        <Route path="/friends" component={Friends} />
-        <Route
-          path="/selectgame"
-          component={!props.token ? Account : GameButtons}
-        />
-        <Route
-          path="/admin"
-          component={
-            props.userData && props.userData.admin.flag ? AdminPanel : Home
-          }
-        />
-        <Redirect to="/notFound" />
+        <Route path='/prueba' component={GameMultiPlayer} />
+        <Route exact path='/' {...props} component={Home} />
+        <Route path='/terms' component={Terms} />
+        <Route path='/privacy' component={Privacy} />
+        <Route path='/notfound' component={NotFound} />
+        <Route path='/game' component={Game} />
+        <Route path='/accounts' component={!props.token ? Account : Home} />
+        <Route path='/friends' component={Friends} />
+        <Route path='/selectgame' component={!props.token ? Account : GameButtons} />
+        <Route path='/admin' component={props.userData && props.userData.admin.flag ? AdminPanel : Home} />
+        <Redirect to='/notFound' />
       </Switch>
     </BrowserRouter>
-  );
-};
+  )
+}
 
 const mapStateToProps = (state) => {
   return {
@@ -124,8 +107,8 @@ const mapStateToProps = (state) => {
     socket: state.users.socket,
     username: state.users.username,
     userData: state.users.userData,
-  };
-};
+  }
+}
 
 const mapDispatchToProps = {
   logInLS: usersActions.logInLS,
@@ -139,6 +122,6 @@ const mapDispatchToProps = {
   // changeCurrentPlayer: socketActions.changeCurrentPlayer,
   // reFetchGameRequests: socketActions.reFetchGameRequests,
   // startGame: socketActions.startGame,
-};
+}
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App)
